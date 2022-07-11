8fbb7a956ba28e193c860401167fc6465dd15947
Pradyut K Bhaumik <pradyut.kumar.bhaumik@gds.ey.com>
2022-07-11 16:20:03 +0530

    chk reference

--- modules/kms/kms.tf
+++ modules/kms/kms.tf
@@ -1,6 +1,21 @@
-resource "google_kms_key_ring" "iag-gdp-storage-encrypt-ring" {
+resource "google_kms_key_ring" "iag-gdp-storage-encrypt-ring1" {
   location = "australia-southeast1"
-  name     = "IAG-GDP-STORAGE-ENCRYPT-RING"
+  name     = "IAG-GDP-STORAGE-ENCRYPT-RING1"
   project  = "gp-ct-sbox-adv-dna"
 }
 
+
+
+
+resource "google_kms_crypto_key" "bq_ds_encrypt" {
+  destroy_scheduled_duration = "7776000s"
+  key_ring                   = "projects/gp-ct-sbox-adv-dna/locations/australia-southeast1/keyRings/${google_kms_key_ring.iag-gdp-storage-encrypt-ring1.name}"
+  name                       = "BQ-DS-ENCRYPT"
+  purpose                    = "ENCRYPT_DECRYPT"
+  rotation_period            = "7776000s"
+
+  version_template {
+    algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
+    protection_level = "SOFTWARE"
+  }
+}
