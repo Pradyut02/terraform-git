e2cad4a9b54de027bb7982b885231445b28bb002
Pradyut K Bhaumik <pradyut.kumar.bhaumik@gds.ey.com>
2022-07-11 14:12:25 +0530

    change rotation

--- modules/kms/key.tf
+++ modules/kms/key.tf
@@ -1,13 +1,12 @@
 resource "google_kms_crypto_key" "ds_encrypt" {
-  destroy_scheduled_duration = "86400s"
+  destroy_scheduled_duration = "7776000s"
   key_ring                   = "projects/gp-ct-sbox-adv-dna/locations/australia-southeast1/keyRings/EY-GP-key"
   name                       = "DS-ENCRYPT"
   purpose                    = "ENCRYPT_DECRYPT"
-  rotation_period            = "86400s"
+  rotation_period            = "7776000s"
 
   version_template {
     algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
     protection_level = "SOFTWARE"
   }
 }
-# terraform import google_kms_crypto_key.storage_encrypt projects/gp-ct-sbox-adv-dna/locations/australia-southeast1/keyRings/EY-GP-key/cryptoKeys/DS-ENCRYPT
